60222a19b767d8e51ed64e9eaacf0b8326243ead
owain lewis <owain@owainlewis.com>
2013-06-21 10:11:14 +0100

    import data from fleetsuite

--- app/Global.scala
+++ app/Global.scala
@@ -1,7 +1,9 @@
 import play.api._
 
-import models._
+import models.{ Role, Employee }
 import anorm._
+
+import lib.{ Import }
 
 object Global extends GlobalSettings {
   override def onStart(app: Application) {
@@ -10,6 +12,17 @@ object Global extends GlobalSettings {
 }
 
 object SeedData {
+
+  // Requires an authentication token from FS!
+  def seedFromFleetsuite(token: String) {
+    val users = Import.parse(token)
+    users.get.foreach { user =>
+      val employee = Employee.build((user.firstname, user.lastname, user.email, "", 1, "", ""))
+      // Only create the user is they don't already exist on the system
+      if (!Employee.findByType("email", email).isDefined)
+        Employee.create(employee)
+    }
+  }
 
   def insert() {
     println("Seeding roles...")
--- app/lib/Import.scala
+++ app/lib/Import.scala
@@ -1,20 +1,36 @@
 package lib
 
+import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
+import java.net.URL
+
 /**
- * Imports existing user data to prepopulate the application
- *
+ * Pull in data from Fleetsuite API. Create users if they are not already in the system.
  *
  */
+
+sealed case class User(account_id: Long, email: String, firstname: String, id: Long, lastname: String)
+
+object User {
+  implicit val userReads  = Json.reads[User]
+}
+
 object Import {
 
-  def loadFile(path: String): String = {
-    //io.Source.fromFile(path)
-    path
+  private val baseUrl = "http://www.fleetsuite.com/api/v1/users?token="
+
+  def buildUrl(token: String) = baseUrl ++ token
+
+  def loadFile(path: String) = {
+    io.Source.fromURL(path).mkString
   }
 
-  def parse(file: String) = {
-    Json.parse(loadFile(file))
+  /**
+   * Get all the data as JSON and parse the result into a seq of Users
+   */
+  def parse(token: String) = {
+    val json = Json.parse(loadFile(buildUrl(token)))
+    Json.fromJson[Seq[User]](json)
   }
 }
